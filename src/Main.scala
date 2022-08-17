1a1c8e919a77c1579f2b6d7166061ce813d8114c
Norbert Kowalik <norbert.kowalik@icloud.com>
2022-08-17 13:11:54 +0200

    Analysing input

--- src/Main.scala
+++ src/Main.scala
@@ -2,6 +2,19 @@ package eu.shooktea.sudoku
 
 object Main {
   def main(args: Array[String]): Unit = {
-    println("Hello, sudoku world!")
+    if (args.length == 0) {
+      println(
+        """Pass sudoku grid in format described in README.md to run solving.
+          |You can use flag --step before sudoku grid to display solution step by step with descriptions.
+          |
+          |You can use flag --test without passing sudoku grid to run all tests.
+          |""".stripMargin)
+    } else if (args.head == "--test") {
+      println("Running tests")
+    } else if (args.head == "--step" && args.length >= 2) {
+      println(s"Running for input ${args(1)} with steps")
+    } else {
+      println(s"Running for input ${args.head}")
+    }
   }
 }
