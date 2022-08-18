93fff581c1ade5b6df9817ce107bad45646eae61
Norbert Kowalik <norbert.kowalik@icloud.com>
2022-08-18 17:02:42 +0200

    Running tests

--- src/Main.scala
+++ src/Main.scala
@@ -2,12 +2,16 @@ package eu.shooktea.sudoku
 
 import strategy.Strategy
 
+import scala.io.Source
+import scala.util.Using
+
 object Main {
   def main(args: Array[String]): Unit = {
     if (args.length == 0) {
       println("Sudoku Solver\nCheck README.md for usage information")
     } else if (args.head == "--test") {
       println("Running tests")
+      runTests()
     } else {
       val input = Grid(args.last.trim)
       val byStep = args contains "--step"
@@ -18,6 +22,25 @@ object Main {
       println(s"Result: ($resultMessage)")
       println(result.convertToString)
     }
+  }
+
+  def runTests(): Unit = Using(Source.fromFile("test.txt")){
+    source => source.getLines()
+      .map(_.trim)
+      .filterNot(_.startsWith("#"))
+      .filterNot(_.isEmpty)
+      .grouped(3)
+      .foreach(entry => runTest(entry.head, entry(1), entry(2)))
+  }
+
+  private def runTest(name: String, input: String, expectedOutput: String): Unit = {
+    print(name + "... ")
+    val grid = Grid(input)
+    val solved = solve(grid, byStep = false, showGrid = false).convertToString
+
+    if (solved == expectedOutput)
+      println("OK")
+    else println("failed")
   }
 
   def solve(grid: Grid, byStep: Boolean, showGrid: Boolean): Grid = {
